41ae262c9c78053e30fc0b9da3eca869df1e2258
Tristan McGinnis <tristanmcginnis26@gmail.com>
2024-05-24 14:52:34 -0500

    slow axis0

--- arm_main/src/Main.cpp
+++ arm_main/src/Main.cpp
@@ -245,7 +245,7 @@ void cmd_check(){
 
           motorList[0].setDuty(args[2].toFloat()*args[4].toFloat()*2.1);//axis1, 2.1x speed multiplier
           delay(1);
-          motorList[1].setDuty(args[2].toFloat()*args[5].toFloat()*2.4);//axis2, 4.4x speed multiplier (higher multiplier because in brake mode) (originally 1.4, up to 4.4 for brake, pulled back down to 2.22)
+          motorList[1].setDuty(args[2].toFloat()*args[5].toFloat()*1.4);//axis2, 4.4x speed multiplier (higher multiplier because in brake mode) (originally 1.4, up to 4.4 for brake, pulled back down to 2.22)
           delay(1);
           motorList[2].setDuty(args[2].toFloat()*args[6].toFloat()*0.9);//axis3, 0.9x speed multiplier
 
@@ -419,11 +419,11 @@ void update_x0()
       //Serial.println("STOPPING AXIS 0");
     }else if(x0_state == 1)
     {
-      axis0.writeMicroseconds(1000);//cw
+      axis0.writeMicroseconds(1100);//cw
       //Serial.println("MOVING AXIS 0 CW");
     }else if(x0_state == -1)
     {
-      axis0.writeMicroseconds(2000);//ccw
+      axis0.writeMicroseconds(1900);//ccw
       //Serial.println("MOVING AXIS 0 CCW");
     }
   }else{
