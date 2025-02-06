ffb73f206dac3f50ad2804dce717a7dca7bd1ac9
David <ds0196@uah.edu>
2025-02-06 12:16:04 -0600

    style: use template

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,101 @@
 /**
- * @file main.cpp
- * @author Tristan McGinnis (tlm0047@uah.edu)
- * @brief Digit PCB's Pico code. Controls end effector.
- * 
+ * @file Template.cpp
+ * @author your name (you@domain.com)
+ * @brief description
+ *
  */
 
-//----------//
-// Includes //
-//----------//
+//------------//
+//  Includes  //
+//------------//
 
 #include <Arduino.h>
-
-#include <vector>
-
 #include "AstraMisc.h"
 #include "project/DIGIT.h"
 
-using namespace std;
 
+//------------//
+//  Settings  //
+//------------//
 
-//-------------//
-// Global vars //
-//-------------//
-
+// Comment out to disable LED blinking
 #define BLINK
-// #define DEBUG
 
 
-// millis value of last control command received
+//---------------------//
+//  Component classes  //
+//---------------------//
+
+
+//----------//
+//  Timing  //
+//----------//
+
+uint32_t lastBlink = 0;
+bool ledState = false;
+
 unsigned long lastCtrlCmd = millis();
-// Whether the last servo control was due to control command (1) or timeout (0)
-bool EFcontrolReal;
-
-// millis value of last LED blink
-unsigned long lastBlink = 0;
-bool ledState = true;
 
 
-//------------//
-// Prototypes //
-//------------//
+//--------------//
+//  Prototypes  //
+//--------------//
 
 
-//-------------//
-// Begin Setup //
-//-------------//
-
+//------------------------------------------------------------------------------------------------//
+//  Setup
+//------------------------------------------------------------------------------------------------//
+//
+//
+//------------------------------------------------//
+//                                                //
+//      ////////    //////////    //////////      //
+//    //                //        //        //    //
+//    //                //        //        //    //
+//      //////          //        //////////      //
+//            //        //        //              //
+//            //        //        //              //
+//    ////////          //        //              //
+//                                                //
+//------------------------------------------------//
 void setup() {
-    //-----------------//
-    // Initialize Pins //
-    //-----------------//
+    //--------//
+    //  Pins  //
+    //--------//
 
-    // Serial and onboard LED
     pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(SERIAL_BAUD);
-    COMMS_UART.begin(COMMS_UART_BAUD);
-
-    // Show with LED that pico is starting
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(2000);
+    delay(1000);
     digitalWrite(LED_BUILTIN, LOW);
 
-    // Laser
-    pinMode(PIN_LASER, OUTPUT);
-    digitalWrite(PIN_LASER, LOW);
+    pinMode(LASER_NMOS, OUTPUT);
+    digitalWrite(LASER_NMOS, LOW);
+
+
+    //------------------//
+    //  Communications  //
+    //------------------//
+
+    Serial.begin(SERIAL_BAUD);
+    Serial1.begin(COMMS_UART_BAUD);
+
+
+    //-----------//
+    //  Sensors  //
+    //-----------//
+
+
+    //--------------------//
+    //  Misc. Components  //
+    //--------------------//
+
+    // LSS will go here
 }
 
 
-//------------//
-// Begin Loop //
-//------------//
+//------------------------------------------------------------------------------------------------//
+//  Loop
+//------------------------------------------------------------------------------------------------//
 //
 //
 //-------------------------------------------------//
@@ -83,9 +109,10 @@ void setup() {
 //    /////////      //////////    //              //
 //                                                 //
 //-------------------------------------------------//
-
 void loop() {
-    // Blink LED
+    //----------//
+    //  Timers  //
+    //----------//
 #ifdef BLINK
     if (millis() - lastBlink > 1000) {
         lastBlink = millis();
@@ -95,33 +122,48 @@ void loop() {
 #endif
 
     // Motor control timeout
-    if (millis() - lastCtrlCmd > 1000) {  // temporarily set to 1 second
+    if (millis() - lastCtrlCmd > 2000) {
         lastCtrlCmd = millis();
-        EFcontrolReal = 0;
+        // Stop LSS
+        // Stop EF motor
+        // Stop lin ac
 #ifdef DEBUG
-        Serial.println("Stopping servo");
+        Serial.println("Control timeout");
 #endif
     }
 
 
-    //-------------------//
-    // Command Receiving //
-    //-------------------//
+    //-------------//
+    //  CAN Input  //
+    //-------------//
 
-    if (COMMS_UART.available()) {
-        String command = COMMS_UART.readStringUntil('\n');
-        command.trim();
-        command.toLowerCase();
-#ifdef DEBUG
-        Serial.print("[IN]: ");
-        Serial.print(command);
-        Serial.print('\n');
-#endif
 
-        std::vector<String> args = {};
-        parseInput(command, args, ',');
+    //------------------//
+    //  UART/USB Input  //
+    //------------------//
+    //
+    //
+    //-------------------------------------------------------//
+    //                                                       //
+    //      /////////    //\\        ////    //////////      //
+    //    //             //  \\    //  //    //        //    //
+    //    //             //    \\//    //    //        //    //
+    //    //             //            //    //        //    //
+    //    //             //            //    //        //    //
+    //    //             //            //    //        //    //
+    //      /////////    //            //    //////////      //
+    //                                                       //
+    //-------------------------------------------------------//
+    if (Serial.available()) {
+        String input = Serial.readStringUntil('\n');
 
-        // Comes from ROS -> socket raspi ->UART-> socket teensy 4.1 ->UART-> Digit
+        input.trim();                   // Remove preceding and trailing whitespace
+        std::vector<String> args = {};  // Initialize empty vector to hold separated arguments
+        parseInput(input, args, ',');   // Separate `input` by commas and place into args vector
+        args[0].toLowerCase();          // Make command case-insensitive
+        String command = args[0];       // To make processing code more readable
+
+        // Backwards compatibility with last year's ROS2 code
         if (command == "digit") {
             // Remove first argument, which is "digit" to tell socket teensy to redirect to digit
             args.erase(args.begin());
@@ -130,13 +172,42 @@ void loop() {
             command.toLowerCase();
         }
 
-        if (args[0] == "ctrl") {  // Is looking for a command that looks like
+        //--------//
+        //  Misc  //
+        //--------//
+        /**/ if (command == "ping") {
+            Serial.println("pong");
+        }
+
+        else if (command == "time") {
+            Serial.println(millis());
+        }
+
+        else if (command == "led") {
+            if (args[1] == "on")
+                digitalWrite(LED_BUILTIN, HIGH);
+            else if (args[1] == "off")
+                digitalWrite(LED_BUILTIN, LOW);
+            else if (args[1] == "toggle") {
+                ledState = !ledState;
+                digitalWrite(LED_BUILTIN, ledState);
+            }
+        }
+
+        //-----------//
+        //  Sensors  //
+        //-----------//
+
+        //----------//
+        //  Motors  //
+        //----------//
+
+        else if (args[0] == "ctrl") {  // Is looking for a command that looks like
                                   // "ctrl,LeftY-Axis,RightY-Axis" where LY,RY are >-1 and <1
 #ifdef DEBUG
             Serial.println("controling");
 #endif
             lastCtrlCmd = millis();
-            EFcontrolReal = 1;
             if (args[1] == "1")  // close
             {
                 // EFcontrol(0, EFcontrolReal);  // 0(close)-2500(open) with 1500 as stop
@@ -150,31 +221,29 @@ void loop() {
 
         } else if (args[0] == "laser") {
             if (args[1] == "0") {
-                digitalWrite(PIN_LASER, LOW);
+                digitalWrite(LASER_NMOS, LOW);
             } else if (args[1] == "1") {
-                digitalWrite(PIN_LASER, HIGH);
+                digitalWrite(LASER_NMOS, HIGH);
             }
 
-        } else if (args[0] == "ping") {
-            Serial.println("pong");
-            COMMS_UART.println("pong");
-        } else if (args[0] == "time") {
-            Serial.println(millis());
         }
     }
-
-    // Take input from USB for debugging
-    if (Serial.available()) {
-        String command = Serial.readStringUntil('\n');
-        command.trim();
-        std::vector<String> args = {};
-        parseInput(command, args, ',');
-#ifdef DEBUG
-        Serial.print("GOT from Serial: ");
-        Serial.println(command);
-        // COMMS_UART.print("faeriesht,");
-        // COMMS_UART.print("hereiscommand,");
-        // COMMS_UART.println(command);
-#endif
-    }
 }
+
+
+//------------------------------------------------------------------------------------------------//
+//  Function definitions
+//------------------------------------------------------------------------------------------------//
+//
+//
+//----------------------------------------------------//
+//                                                    //
+//    //////////    //          //      //////////    //
+//    //            //\\        //    //              //
+//    //            //  \\      //    //              //
+//    //////        //    \\    //    //              //
+//    //            //      \\  //    //              //
+//    //            //        \\//    //              //
+//    //            //          //      //////////    //
+//                                                    //
+//----------------------------------------------------//
