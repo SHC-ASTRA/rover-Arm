b90f332ea45ec2131cd9012907b15d0369c57bf3
Tonez3D <tonez3d@gmail.com>
2026-02-17 20:56:32 -0600

    Update platformio.ini & added dev flake

---
+++ arm_main/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Dev flake for C++ Embedded development with Platform IO";
+  inputs = { 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+  outputs = inputs:
+  let 
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+      forEachSupportedSystem = f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+            inherit system;
+          });
+  in {
+    devShells = forEachSupportedSystem ({ pkgs, system }: {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python3
+        ];
+      };
+    });
+  };
+}
